08a702a14419f5b36faf808dcb1724dfe0a808da
Stephen-Ibe <saintkalson@gmail.com>
2023-01-08 09:39:21 +0100

    404 Page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 type Props = {};
 
 const NotFound = (props: Props) => {
-  return <div>NotFound</div>;
+  return (
+    <div className='container flex flex-col items-center justify-center h-screen mx-auto text-center border'>
+      <h1 className='text-6xl'>404</h1>
+      <p className='uppercase'>Page not found</p>
+      <Link
+        to='/'
+        className='px-8 py-2 my-4 font-semibold text-white bg-blue-400 border-none rounded-lg'
+      >
+        Return Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
