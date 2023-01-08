524ac274c850881d75bc2e18aae6ac09d0750d82
Stephen-Ibe <saintkalson@gmail.com>
2023-01-08 18:03:54 +0100

    product page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -22,8 +22,8 @@ const Products = (props: Props) => {
 
   return (
     <div className='chi-container'>
-      <div className='py-4 text-center border-b-2'>
-        <h1 className='text-xl font-semibold'>
+      <div className='py-8 text-center border-b-2'>
+        <h1 className='text-3xl font-semibold'>
           Shop for the perfect gift card
         </h1>
       </div>
@@ -40,6 +40,31 @@ const Products = (props: Props) => {
           </li>
         </ul>
       </nav>
+      <section className='p-8 my-16 products'>
+        <div className='grid gap-8 gap-y-24 gid-cols-1 lg:grid-cols-4 md:grid-cols-2'>
+          {giftCards?.content?.length > 0 ? (
+            giftCards?.content?.map((giftCard: any) => (
+              <div className='border-4'>
+                <div className='relative w-full h-[200px]'>
+                  <img
+                    src={giftCard.img}
+                    alt='giftCard_image'
+                    className='absolute object-cover w-full h-full'
+                  />
+                </div>
+                <div>
+                  <h4 className='text-base font-semibold'>
+                    {giftCard.productName}
+                  </h4>
+                  <p className='text-sm'>{giftCard.description}</p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p className='text-center'>No Gift Cards Founds</p>
+          )}
+        </div>
+      </section>
     </div>
   );
 };
