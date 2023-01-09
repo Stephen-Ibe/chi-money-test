ea36e5e904c9e6e859fd85270ddd1d26d96fb621
Stephen-Ibe <saintkalson@gmail.com>
2023-01-09 11:51:51 +0100

    product details and style update

--- src/pages/products/ProductDetails.tsx
+++ src/pages/products/ProductDetails.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../../components/blocks/navbar';
 import { useLocation } from 'react-router-dom';
 import formatNumber from '../../utils/formatNumber';
+import ChiButton from '../../components/common/button';
 
 const ProductDetails = () => {
   const { state } = useLocation();
@@ -21,12 +22,40 @@ const ProductDetails = () => {
               />
             </div>
           </div>
-          <div className='w-8/12 p-6 card_deatils'>
+          <div className='w-8/12 p-6 card_details'>
             <h1 className='text-2xl font-semibold'>{state.productName}</h1>
-            <p>Brand:{state?.brand?.brandName}</p>
-            <p>Type:{state?.type}</p>
+            <p>Brand: {state?.brand?.brandName}</p>
+            <p>Type: {state?.type}</p>
             <div>
-              <p>Price: ${formatNumber(state?.senderFee * 100)}</p>
+              <p className='flex'>
+                Price:
+                <span className='text-base font-semibold text-red-800'>
+                  ${formatNumber(state?.senderFee * 100)}
+                </span>
+              </p>
+              <p>Gift Amount ($50 - $2000)</p>
+              <div className='my-2 space-x-2'>
+                <ChiButton
+                  title='$25'
+                  className='px-4 py-1 text-sm font-semibold border border-black rounded btn'
+                />
+                <ChiButton
+                  title='$50'
+                  className='px-4 py-1 text-sm font-semibold border border-black rounded btn'
+                />
+                <ChiButton
+                  title='$100'
+                  className='px-4 py-1 text-sm font-semibold border border-black rounded btn'
+                />
+                <ChiButton
+                  title='$150'
+                  className='px-4 py-1 text-sm font-semibold border border-black rounded btn'
+                />
+                <ChiButton
+                  title='$500'
+                  className='px-4 py-1 text-sm font-semibold border border-black rounded btn'
+                />
+              </div>
             </div>
           </div>
         </section>
